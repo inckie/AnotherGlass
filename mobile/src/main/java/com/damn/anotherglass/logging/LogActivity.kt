8478a20385ba65ad2a2eab198833204763739d69
Ink <i@am.com>
2024-04-05 23:52:11 -0400

    * use auto-bind pager in x-ray Activity

--- mobile/src/main/java/com/damn/anotherglass/logging/LogActivity.kt
+++ mobile/src/main/java/com/damn/anotherglass/logging/LogActivity.kt
@@ -1,16 +1,12 @@
 package com.damn.anotherglass.logging
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.viewpager.widget.ViewPager
-import com.applicaster.xray.ui.adapters.ViewsPagerAdapter
+import androidx.appcompat.app.AppCompatActivity
 import com.damn.anotherglass.R
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
-        val pager: ViewPager = findViewById(R.id.pager)
-        pager.adapter = ViewsPagerAdapter(pager)
     }
 }
