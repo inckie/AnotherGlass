a444f1d2b45858a3fbec5c1537e18dc0e334f6a4
Ink <i@am.com>
2024-04-06 14:06:26 -0400

    * Kotlinize MainActivity

--- glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/MainActivity.kt
+++ glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/MainActivity.kt
@@ -13,87 +13,77 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.damn.anotherglass.glass.ee.host
 
-package com.damn.anotherglass.glass.ee.host;
-
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import com.damn.anotherglass.glass.ee.host.fragments.BaseFragment;
-import com.damn.anotherglass.glass.ee.host.fragments.ColumnLayoutFragment;
-import com.damn.anotherglass.glass.ee.host.fragments.MainLayoutFragment;
-import com.example.glass.ui.GlassGestureDetector.Gesture;
-import com.google.android.material.tabs.TabLayout;
-import java.util.ArrayList;
-import java.util.List;
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.damn.anotherglass.glass.ee.host.fragments.BaseFragment
+import com.damn.anotherglass.glass.ee.host.fragments.ColumnLayoutFragment
+import com.damn.anotherglass.glass.ee.host.fragments.MainLayoutFragment
+import com.example.glass.ui.GlassGestureDetector
+import com.google.android.material.tabs.TabLayout
 
 /**
  * Main activity of the application. It provides viewPager to move between fragments.
  */
-public class MainActivity extends BaseActivity {
+class MainActivity : BaseActivity() {
+    private val fragments: MutableList<BaseFragment> = ArrayList()
+    private lateinit var viewPager: ViewPager
 
-    private final List<BaseFragment> fragments = new ArrayList<>();
-    private ViewPager viewPager;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.view_pager_layout)
+        viewPager = findViewById(R.id.viewPager)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_pager_layout);
+        fragments.add(
+            MainLayoutFragment.newInstance(
+                getString(R.string.text_sample), getString(R.string.footnote_sample),
+                getString(R.string.timestamp_sample), null
+            )
+        )
+        fragments.add(
+            MainLayoutFragment.newInstance(
+                getString(R.string.different_options), getString(R.string.empty_string),
+                getString(R.string.empty_string), R.menu.main_menu
+            )
+        )
+        fragments.add(
+            ColumnLayoutFragment
+                .newInstance(
+                    R.drawable.ic_style, getString(R.string.columns_sample),
+                    getString(R.string.footnote_sample), getString(R.string.timestamp_sample)
+                )
+        )
+        fragments.add(
+            MainLayoutFragment.newInstance(
+                getString(R.string.like_this_sample), getString(R.string.empty_string),
+                getString(R.string.empty_string), null
+            )
+        )
+        viewPager.setAdapter(ScreenSlidePagerAdapter(supportFragmentManager))
 
-        final ScreenSlidePagerAdapter screenSlidePagerAdapter = new ScreenSlidePagerAdapter(
-            getSupportFragmentManager());
-        viewPager = findViewById(R.id.viewPager);
-        viewPager.setAdapter(screenSlidePagerAdapter);
-
-        fragments.add(MainLayoutFragment
-            .newInstance(getString(R.string.text_sample), getString(R.string.footnote_sample),
-                getString(R.string.timestamp_sample), null));
-        fragments.add(MainLayoutFragment
-            .newInstance(getString(R.string.different_options), getString(R.string.empty_string),
-                getString(R.string.empty_string), R.menu.main_menu));
-        fragments.add(ColumnLayoutFragment
-            .newInstance(R.drawable.ic_style, getString(R.string.columns_sample),
-                getString(R.string.footnote_sample), getString(R.string.timestamp_sample)));
-        fragments.add(MainLayoutFragment
-            .newInstance(getString(R.string.like_this_sample), getString(R.string.empty_string),
-                getString(R.string.empty_string), null));
-
-        screenSlidePagerAdapter.notifyDataSetChanged();
-
-        final TabLayout tabLayout = findViewById(R.id.page_indicator);
-        tabLayout.setupWithViewPager(viewPager, true);
+        val tabLayout = findViewById<TabLayout>(R.id.page_indicator)
+        tabLayout.setupWithViewPager(viewPager, true)
     }
 
-    @Override
-    public boolean onGesture(Gesture gesture) {
-        switch (gesture) {
-            case TAP:
-                fragments.get(viewPager.getCurrentItem()).onSingleTapUp();
-                return true;
-            default:
-                return super.onGesture(gesture);
-        }
-    }
+    override fun onGesture(gesture: GlassGestureDetector.Gesture): Boolean =
+        when (gesture) {
+            GlassGestureDetector.Gesture.TAP -> {
+                fragments[viewPager.currentItem].onSingleTapUp()
+                true
+            }
 
-    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
-
-        ScreenSlidePagerAdapter(FragmentManager fm) {
-            super(fm);
+            else -> super.onGesture(gesture)
         }
 
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            return fragments.get(position);
-        }
+    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) :
+        FragmentStatePagerAdapter(fm) {
 
-        @Override
-        public int getCount() {
-            return fragments.size();
-        }
+        override fun getItem(position: Int): Fragment = fragments[position]
+
+        override fun getCount(): Int = fragments.size
     }
 }
