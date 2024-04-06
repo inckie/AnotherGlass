aa01c1391a854d526fb133a2eb8903cd7fd4372f
Ink <i@am.com>
2024-04-06 13:31:40 -0400

    * migrate GlassMenuItem to Kotlin

--- glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/menu/GlassMenuItem.kt
+++ glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/menu/GlassMenuItem.kt
@@ -13,79 +13,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.damn.anotherglass.glass.ee.host.menu
 
-package com.damn.anotherglass.glass.ee.host.menu;
-
-import android.graphics.drawable.Drawable;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import java.util.Objects;
+import android.graphics.drawable.Drawable
 
 /**
  * Represents the single menu item object.
  */
-public class GlassMenuItem {
 
-  private int id;
-  private Drawable icon;
-  private String text;
-
-  /**
-   * {@link GlassMenuItem} object is constructed by usage of this method.
-   *
-   * @param id is an id of the the current menu item.
-   * @param icon is a menu icon {@link Drawable} object.
-   * @param text is a String with the menu option label.
-   */
-  GlassMenuItem(int id, Drawable icon, String text) {
-    this.id = id;
-    this.icon = icon;
-    this.text = text;
-  }
-
-  /**
-   * Returns menu item id.
-   */
-  public int getId() {
-    return id;
-  }
-
-  /**
-   * Returns menu item icon.
-   */
-  public Drawable getIcon() {
-    return icon;
-  }
-
-  /**
-   * Returns menu item label.
-   */
-  public String getText() {
-    return text;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(id, icon, text);
-  }
-
-  @Override
-  public boolean equals(@Nullable Object other) {
-    if (this == other) {
-      return true;
-    }
-    if (!(other instanceof GlassMenuItem)) {
-      return false;
-    }
-    final GlassMenuItem that = (GlassMenuItem) other;
-    return Objects.equals(this.id, that.id) &&
-        Objects.equals(this.icon, that.icon) &&
-        Objects.equals(this.text, that.text);
-  }
-
-  @NonNull
-  @Override
-  public String toString() {
-    return id + " " + icon + " " + text;
-  }
-}
+/**
+ * [GlassMenuItem] object is constructed by usage of this method.
+ *
+ * @param id is an id of the the current menu item.
+ * @param icon is a menu icon [Drawable] object.
+ * @param text is a String with the menu option label.
+ **/
+data class GlassMenuItem(
+    val id: Int,
+    val icon: Drawable,
+    val text: String
+)
