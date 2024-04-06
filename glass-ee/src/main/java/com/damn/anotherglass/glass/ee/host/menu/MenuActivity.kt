3efe89f95258b65feb03edcba7eda37be132ccb3
Ink <i@am.com>
2024-04-06 13:55:20 -0400

    * Kotlinize Menu code

--- glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/menu/MenuActivity.kt
+++ glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/menu/MenuActivity.kt
@@ -13,112 +13,95 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.damn.anotherglass.glass.ee.host.menu
 
-package com.damn.anotherglass.glass.ee.host.menu;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.PagerSnapHelper;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.recyclerview.widget.RecyclerView.LayoutManager;
-import androidx.recyclerview.widget.RecyclerView.OnScrollListener;
-import androidx.recyclerview.widget.SnapHelper;
-import com.damn.anotherglass.glass.ee.host.BaseActivity;
-import com.damn.anotherglass.glass.ee.host.R;
-import com.example.glass.ui.GlassGestureDetector.Gesture;
-import com.example.glass.ui.GlassGestureDetector.OnGestureListener;
-import java.util.ArrayList;
-import java.util.List;
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
+import com.damn.anotherglass.glass.ee.host.BaseActivity
+import com.damn.anotherglass.glass.ee.host.R
+import com.example.glass.ui.GlassGestureDetector
 
 /**
  * Activity which provides the menu functionality. It creates the horizontal recycler view to move
  * between menu items.
  */
-public class MenuActivity extends BaseActivity implements OnGestureListener {
+class MenuActivity : BaseActivity(), GlassGestureDetector.OnGestureListener {
+    private var adapter: MenuAdapter? = null
+    private val menuItems: MutableList<GlassMenuItem> = ArrayList()
+    private var currentMenuItemIndex = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_layout)
+        val recyclerView = findViewById<RecyclerView>(R.id.menuRecyclerView)
+        adapter = MenuAdapter(menuItems)
+        val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL, false
+        )
+        recyclerView.setLayoutManager(layoutManager)
+        recyclerView.setAdapter(adapter)
+        recyclerView.isFocusable = true
+        val snapHelper: SnapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerView)
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val foundView = snapHelper.findSnapView(layoutManager) ?: return
+                currentMenuItemIndex = layoutManager.getPosition(foundView)
+            }
+        })
+    }
 
-  /**
-   * Key for the menu item id.
-   */
-  public static final String EXTRA_MENU_ITEM_ID_KEY = "id";
-
-  /**
-   * Default value for the menu item.
-   */
-  public static final int EXTRA_MENU_ITEM_DEFAULT_VALUE = -1;
-
-  /**
-   * Key for the menu.
-   */
-  public static final String EXTRA_MENU_KEY = "menu_key";
-
-  private MenuAdapter adapter;
-  private final List<GlassMenuItem> menuItems = new ArrayList<>();
-  private int currentMenuItemIndex;
-
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.menu_layout);
-    final RecyclerView recyclerView = findViewById(R.id.menuRecyclerView);
-    adapter = new MenuAdapter(menuItems);
-    final LayoutManager layoutManager = new LinearLayoutManager(this,
-        LinearLayoutManager.HORIZONTAL, false);
-    recyclerView.setLayoutManager(layoutManager);
-    recyclerView.setAdapter(adapter);
-    recyclerView.setFocusable(true);
-
-    final SnapHelper snapHelper = new PagerSnapHelper();
-    snapHelper.attachToRecyclerView(recyclerView);
-
-    recyclerView.addOnScrollListener(new OnScrollListener() {
-      @Override
-      public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-        final View foundView = snapHelper.findSnapView(layoutManager);
-        if (foundView == null) {
-          return;
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val menuResource = intent.getIntExtra(EXTRA_MENU_KEY, EXTRA_MENU_ITEM_DEFAULT_VALUE)
+        if (menuResource != EXTRA_MENU_ITEM_DEFAULT_VALUE) {
+            menuInflater.inflate(menuResource, menu)
+            for (i in 0 until menu.size()) {
+                val menuItem = menu.getItem(i)
+                menuItems.add(
+                    GlassMenuItem(
+                        menuItem.itemId,
+                        menuItem.icon!!,
+                        menuItem.title.toString()
+                    )
+                )
+                adapter!!.notifyDataSetChanged()
+            }
         }
-        currentMenuItemIndex = layoutManager.getPosition(foundView);
-      }
-    });
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    final int menuResource = getIntent()
-        .getIntExtra(EXTRA_MENU_KEY, EXTRA_MENU_ITEM_DEFAULT_VALUE);
-    if (menuResource != EXTRA_MENU_ITEM_DEFAULT_VALUE) {
-      final MenuInflater inflater = getMenuInflater();
-      inflater.inflate(menuResource, menu);
-
-      for (int i = 0; i < menu.size(); i++) {
-        final MenuItem menuItem = menu.getItem(i);
-        menuItems.add(
-            new GlassMenuItem(menuItem.getItemId(), menuItem.getIcon(),
-                menuItem.getTitle().toString()));
-        adapter.notifyDataSetChanged();
-      }
+        return super.onCreateOptionsMenu(menu)
     }
-    return super.onCreateOptionsMenu(menu);
-  }
 
-  @Override
-  public boolean onGesture(Gesture gesture) {
-    switch (gesture) {
-      case TAP:
-        final Intent intent = new Intent();
-        intent.putExtra(EXTRA_MENU_ITEM_ID_KEY, menuItems.get(currentMenuItemIndex).getId());
-        setResult(RESULT_OK, intent);
-        finish();
-        return true;
-      default:
-        return super.onGesture(gesture);
+    override fun onGesture(gesture: GlassGestureDetector.Gesture): Boolean {
+        return when (gesture) {
+            GlassGestureDetector.Gesture.TAP -> {
+                val intent = Intent()
+                    .putExtra(EXTRA_MENU_ITEM_ID_KEY, menuItems[currentMenuItemIndex].id)
+                setResult(RESULT_OK, intent)
+                finish()
+                true
+            }
+            else -> super.onGesture(gesture)
+        }
     }
-  }
+
+    companion object {
+        /**
+         * Key for the menu item id.
+         */
+        const val EXTRA_MENU_ITEM_ID_KEY = "id"
+
+        /**
+         * Default value for the menu item.
+         */
+        const val EXTRA_MENU_ITEM_DEFAULT_VALUE = -1
+
+        /**
+         * Key for the menu.
+         */
+        const val EXTRA_MENU_KEY = "menu_key"
+    }
 }
--- glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/menu/MenuAdapter.kt
+++ glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/menu/MenuAdapter.kt
@@ -13,56 +13,32 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.damn.anotherglass.glass.ee.host.menu
 
-package com.damn.anotherglass.glass.ee.host.menu;
-
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-import com.damn.anotherglass.glass.ee.host.databinding.MenuItemBinding;
-import java.util.List;
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.damn.anotherglass.glass.ee.host.databinding.MenuItemBinding
+import com.damn.anotherglass.glass.ee.host.menu.MenuAdapter.MenuViewHolder
 
 /**
  * Adapter for the menu horizontal recycler view.
  */
-public class MenuAdapter extends RecyclerView.Adapter<MenuAdapter.MenuViewHolder> {
+class MenuAdapter internal constructor(private val menuItems: List<GlassMenuItem>) :
+    RecyclerView.Adapter<MenuViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder =
+        MenuViewHolder(MenuItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
-  private final List<GlassMenuItem> menuItems;
+    override fun onBindViewHolder(menuViewHolder: MenuViewHolder, position: Int) =
+        menuViewHolder.bind(menuItems[position])
 
-  MenuAdapter(List<GlassMenuItem> menuItems) {
-    this.menuItems = menuItems;
-  }
+    override fun getItemCount(): Int = menuItems.size
 
-  @NonNull
-  @Override
-  public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-    return new MenuViewHolder(MenuItemBinding.inflate(
-        LayoutInflater.from(parent.getContext()), parent, false));
-  }
-
-  @Override
-  public void onBindViewHolder(@NonNull MenuViewHolder menuViewHolder, int position) {
-    menuViewHolder.bind(menuItems.get(position));
-  }
-
-  @Override
-  public int getItemCount() {
-    return menuItems.size();
-  }
-
-  public static class MenuViewHolder extends RecyclerView.ViewHolder {
-
-    private final MenuItemBinding binding;
-
-    public MenuViewHolder(@NonNull MenuItemBinding binding) {
-      super(binding.getRoot());
-      this.binding = binding;
+    class MenuViewHolder(private val binding: MenuItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
+        fun bind(glassMenuItem: GlassMenuItem?) {
+            binding.setItem(glassMenuItem)
+            binding.executePendingBindings()
+        }
     }
-
-    public void bind(GlassMenuItem glassMenuItem) {
-      binding.setItem(glassMenuItem);
-      binding.executePendingBindings();
-    }
-  }
 }
