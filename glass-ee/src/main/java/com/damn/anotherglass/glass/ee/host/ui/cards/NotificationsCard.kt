4da1252e9575da23c648b64363f7231be8f19484
Ink <i@am.com>
2024-12-29 16:11:09 -0500

    + Show icon on notification card

--- glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/ui/cards/NotificationsCard.kt
+++ glass-ee/src/main/java/com/damn/anotherglass/glass/ee/host/ui/cards/NotificationsCard.kt
@@ -1,13 +1,16 @@
 package com.damn.anotherglass.glass.ee.host.ui.cards
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.text.format.DateUtils
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.damn.anotherglass.glass.ee.host.core.NotificationController
 import com.damn.anotherglass.glass.ee.host.databinding.LayoutNotificationsStackBinding
+import com.damn.anotherglass.shared.notifications.NotificationData
 
 class NotificationsCard : BaseFragment() {
 
@@ -31,11 +34,35 @@ class NotificationsCard : BaseFragment() {
         NotificationController.instance.getNotifications().observe(this) {
             if (it.isEmpty()) return@observe  // we are about to be removed
             binding?.apply {
-                val last = it.last()
-                title.text = last.title
-                text.text = last.text
-                footer.text = if (it.size > 1) "" + it.size else last.tickerText
-                timestamp.text = DateUtils.formatDateTime(context, last.postedTime, DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_ABBREV_ALL)
+                it.last()
+                bindData(it.last(), context)
+                // todo: add another place for counter
+                if (it.size > 1) footer.text = "" + it.size
+            }
+        }
+    }
+
+    private fun LayoutNotificationsStackBinding.bindData(
+        last: NotificationData,
+        context: Context
+    ) {
+        title.text = last.title
+        text.text = last.text
+        footer.text = last.tickerText
+        timestamp.text = DateUtils.formatDateTime(
+            context,
+            last.postedTime,
+            DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_SHOW_TIME or DateUtils.FORMAT_ABBREV_ALL
+        )
+        if (last.icon == null) {
+            imgIcon.setImageResource(android.R.drawable.ic_dialog_info)
+        } else {
+            try {
+                val bitmap = BitmapFactory.decodeByteArray(last.icon, 0, last.icon.size)
+                imgIcon.setImageBitmap(bitmap)
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to decode icon: " + e.message, e)
+                imgIcon.setImageResource(android.R.drawable.ic_dialog_info)
             }
         }
     }
