31481b4099d297a5b91815a58ad8ae002fa1a390
Ink <i@am.com>
2025-06-29 00:13:33 -0400

    + LRU on AndroidAppDetailsProvider

--- mobile/src/main/java/com/damn/anotherglass/utility/AppDetailsProvider.kt
+++ mobile/src/main/java/com/damn/anotherglass/utility/AppDetailsProvider.kt
@@ -1,9 +1,9 @@
 package com.damn.anotherglass.utility
 
 import android.content.Context
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.graphics.drawable.Drawable
+import androidx.collection.LruCache
 
 data class AppDetails(val appName: String, val icon: Drawable?)
 
@@ -12,15 +12,19 @@ interface AppDetailsProvider {
 }
 
 class AndroidAppDetailsProvider(private val context: Context) : AppDetailsProvider {
-    override fun getAppDetails(packageName: String): AppDetails {
-        val pm = context.packageManager
-        return try {
-            val appInfo: ApplicationInfo = pm.getApplicationInfo(packageName, 0)
+
+    private val cache: LruCache<String, AppDetails> = object : LruCache<String, AppDetails>(64) {
+        override fun create(key: String): AppDetails? = try {
+            val pm = context.packageManager
+            val appInfo = pm.getApplicationInfo(key, 0)
             val appName = pm.getApplicationLabel(appInfo).toString()
             val icon = pm.getApplicationIcon(appInfo)
             AppDetails(appName, icon)
         } catch (e: PackageManager.NameNotFoundException) {
-            AppDetails(packageName, null) // Return package name if not found
+            null // unlikely case of a package uninstalled after posting a notification
         }
     }
+
+    override fun getAppDetails(packageName: String): AppDetails =
+        cache[packageName] ?: AppDetails(packageName, null)
 }
